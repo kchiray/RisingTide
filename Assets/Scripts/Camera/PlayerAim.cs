642f6bbf6487d69c25080dd6cd5a42d919a375ad
kchiray <Batman@DESKTOP-O8058I2>
2018-07-14 16:37:20 -0400

    walking add

---
+++ Assets/Scripts/Camera/PlayerAim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10.0f;
+    // Use this for initialization
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+
+    void Update()
+    {
+        CameraMove();
+    }
+
+    public void CameraMove()
+    {
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+        }
+
+        else if (Input.GetAxis("Mouse X") < 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+        }
+    }
+}
